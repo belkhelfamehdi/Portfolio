55aa0b17ca91cbb4b180f630a5b99884ec05791a
SparkyDZZ <124880239+SparkyDZZ@users.noreply.github.com>
2024-11-10 20:19:09 +0100

    Init commit

--- src/components/ui/NavLinks.jsx
+++ src/components/ui/NavLinks.jsx
@@ -5,6 +5,7 @@ const NavLinks = () => {
         <ul className="sm:flex max-sm:flex-col items-center space-y-4 sm:space-y-0 sm:space-x-8 font-poppins text-md font-bold text-black dark:text-white transition-colors duration-300">
             <li><a href="#home" className={link}>Home</a></li>
             <li><a href="#about" className={link}>About</a></li>
+            <li><a href="#formations" className={link}>Education</a></li>
             <li><a href="#skills" className={link}>Skills</a></li>
             <li><a href="#projects" className={link}>Projects</a></li>
             <li><a href="#contact" className={link}>Contact</a></li>
