2d3f8480d450fae29dafce6e4e54f6273004ad3d
Mehdi Belkhelfa <midou8687@gmail.com>
2025-04-09 10:21:31 +0200

    Update homepage URL in package.json; modify Logo component link to point to /Portfolio

--- src/components/page-ui/Navbar.jsx
+++ src/components/page-ui/Navbar.jsx
@@ -15,7 +15,6 @@ const Navbar = () => {
         <div className='flex items-center justify-center dark:bg-[#1f1f1f] bg-white dark:shadow-[#14191b] shadow-md fixed top-0 w-full z-40 transition-all duration-500'>
             <nav className="md:container w-full flex items-center justify-between py-3 px-6 md:px-24">
                 <Logo />
-                {/* Desktop Links */}
                 <div className="hidden sm:flex justify-between w-full items-center space-x-8">
                     <div className="sm:invisible">
                         <Logo />
@@ -23,7 +22,6 @@ const Navbar = () => {
                     <NavLinks />
                     <DarkModeToggle />
                 </div>
-                {/* Mobile Hamburger Menu */}
                 <button className="sm:hidden z-40" onClick={toggleMenu}>
                     {isMenuOpen ? (
                         <HiX className='text-4xl dark:text-white text-black' />
@@ -32,7 +30,6 @@ const Navbar = () => {
                     )}
                 </button>
             </nav>
-            {/* Mobile Slide-In Menu */}
             <div
                 className={`sm:hidden fixed top-0 right-0 h-full bg-white dark:bg-[#1f1f1f] shadow-lg space-y-10 py-4 px-24 z-30 transform transition-all duration-500 ease-in-out ${
                     isMenuOpen ? 'translate-x-0' : 'translate-x-full'
@@ -41,7 +38,6 @@ const Navbar = () => {
                 <DarkModeToggle />
                 <NavLinks />
             </div>
-            {/* Overlay to close menu when clicking outside */}
             {isMenuOpen && (
                 <div
                     className="fixed inset-0 bg-black opacity-50 z-20"
--- src/components/ui/Logo.jsx
+++ src/components/ui/Logo.jsx
@@ -4,7 +4,7 @@ import { FaCode } from "react-icons/fa6";
 const Logo = () => {
     return (
         <div className="flex items-center justify-center">
-            <Link to="/">
+            <Link to="/Portfolio">
                 <FaCode className="text-4xl text-[#cf1b1b]" />
             </Link>
         </div>
